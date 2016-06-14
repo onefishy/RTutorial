b4003df0aab894e8365b24a28069cb1c8b7d868b
Irving Pena <Irving Pena>
2016-06-14 17:12:53 -0400

    updated UI. Working on understanding reactive values in server()

--- houseUI.r
+++ houseUI.r
@@ -26,14 +26,47 @@ ui <- shinyUI(fluidPage(
                   max = 50,
                   value = 30),
       
-      selectInput( inputId = "cities",
-                   label = "Location",
-                   choices = levels(mydata$Location),
-                   multiple = FALSE,
-                   selectize = FALSE,
-                   size = length(levels(mydata$Location))
-                   )
+      sliderInput(inputId = "price",
+                  label   = "Price",
+                  min     = 1,
+                  max     = max(mydata$Price),
+                  value   = sample(mydata$Price, 1)), # returns 1 random element of mydata$price
       
+      numericInput( inputId = "beds",
+                    label   = "# of bedrooms",
+                    min     = min(mydata$Bedrooms),
+                    max     = max(mydata$Bedrooms),
+                    value   = sample(mydata$Bedrooms, 1)),
+      
+      numericInput( inputId = "baths",
+                    label   = "# of bathrooms",
+                    min     = min(mydata$Bathrooms, na.rm = TRUE),
+                    max     = max(mydata$Bathrooms, na.rm = TRUE),
+                    value   = sample(mydata$Bathrooms, 1)),
+      
+      sliderInput( inputId = "sqft",
+                   label   = "Sq. ft.",
+                   min     = min(mydata$Sqft, na.rm = TRUE),
+                   max     = max(mydata$Sqft, na.rm = TRUE),
+                   value   = sample(mydata$Sqft, 1)),
+      
+      sliderInput( inputId = "pp_sq_ft",
+                   label   = "Sq. ft.",
+                   min     = min(mydata$Price.Sqft, na.rm = TRUE),
+                   max     = max(mydata$Price.Sqft, na.rm = TRUE),
+                   value   = sample(mydata$Price.Sqft, 1)),
+      
+      selectInput( inputId = "sale_type",
+                   label   = "Sale Type",
+                   choices = levels(mydata$Status)),
+    
+    
+      selectInput( inputId   = "cities",
+                 label     = "Location",
+                 choices   = levels(mydata$Location),
+                 multiple  = FALSE,
+                 selectize = FALSE,
+                 size      = length(levels(mydata$Location)))
     ),
     
     # Show a plot of the generated distribution
@@ -51,25 +84,45 @@ server <- shinyServer(function(input, output) {
   #setwd('~/RTutorial/Data')
   #mydata = read.csv("California_Home_Prices_(2009).csv")
   
+  city       = reactive({input$cities})
+  max_price  = reactive({input$price})
+  sale_type  = reactive({input$sale_type})
+  sqft       = reactive({input$sqft})
+  baths      = reactive({input$baths})
+  beds       = reactive({input$beds})
+  price_sqft = reactive({input$pp_sq_ft})
+  
+  relevant_data <- reactive({
+    subset(mydata,
+           subset = 
+             Location   == city() &
+             Price      <= max_price() &
+             Bedrooms   == beds() &
+             Bathrooms  == baths() &
+             Sqft       == sqft() &
+             Price.Sqft == price_sqft() &
+             Status     == sale_type() )
+  })
+  
+  
+  # Histogram code
   output$histPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    prices    <- reactive({subset(mydata,
-                        subset = Location == input$cities,
-                        select = Price) })
-    bins <- reactive({seq(min(prices), max(prices), length.out = input$bins + 1)})
+    prices    <- relevant_data()$Price
+    old_prices   <- mydata$Price
+    bins <- seq(0, input$price, length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
     hist(prices, breaks = bins, col = 'darkgray', border = 'white')
   })
   
-  
-##  output$scatterPlot <- renderPlot({
-##    #scatter plot
-##    plot(mydata$Sqft, mydata$Price, main="Price vs Sqft", col=rgb(1,0,0,0.5),
-##         xlab="Sqft", ylab="Price", pch=19)
-##    
-##  })
+  output$scatterPlot <- renderPlot({
+    #scatter plot
+    plot(relevant_data()$Sqft, relevant_data()$Price, main="Price vs Sqft", col=rgb(1,0,0,0.5),
+         xlab="Sqft", ylab="Price", pch=19)
+    
+  })
 })
 
 # Run the application 
